50280ef0964cd168f76637d9d61c962e42755f2a
Julia Nowicka <julia.nowickaa@gmail.com>
2023-03-22 16:49:30 +0100

    Solve a task - values and variables

--- js-fundamentals-part1/script.js
+++ js-fundamentals-part1/script.js
@@ -1,3 +1,20 @@
 let js = 'amazing';
 if(js === 'amazing') alert('JavaScript is FUN!');
 console.log(40 + 8 + 23 -10);
+
+//Values and variables
+let firstName = "Matilda";
+console.log(firstName)
+
+let jonas_matilda = "JM";
+let $function = 27;
+let name = "Jonas";
+let pi = 3.1415;
+
+let myFirstJob = "Flight attendant";
+let myCurrentJob = "Acrobat";
+
+let job1 = "flight attendant";
+let job2 = "acrobat";
+
+console.log(myFirstJob);
