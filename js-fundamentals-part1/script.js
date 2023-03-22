e7eed52faaeb61ae0fdb238e5ee90765111d4fbf
Julia Nowicka <julia.nowickaa@gmail.com>
2023-03-22 19:51:19 +0100

    Add tasks from Data Types lecture

--- js-fundamentals-part1/script.js
+++ js-fundamentals-part1/script.js
@@ -18,9 +18,11 @@ let job1 = "flight attendant";
 let job2 = "acrobat";
 
 console.log(myFirstJob);
+
 /*1. Declare variables called 'country', 'continent' and 'population' and
 assign their values according to your own country (population in millions)
 2. Log their values to the console*/
+
 let country = 'Poland';
 let continent = 'Europe';
 let population = 37.75;
@@ -28,5 +30,38 @@ console.log(country);
 console.log(continent);
 console.log(population);
 
-
 //Data Types
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javaScriptIsFun = "Yes!";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+yeaar = 1920;
+console.log(typeof year);
+
+console.log(typeof null);
+
+/*LECTURE: Data Types
+1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console */
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
