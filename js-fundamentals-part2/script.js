2ee4c74ff2dc12a33ea00a432596aa240ff724da
Julia Nowicka <julia.nowickaa@gmail.com>
2023-04-26 17:26:01 +0200

    Add task solution from "Introduction to Objects" Lecture

--- js-fundamentals-part2/script.js
+++ js-fundamentals-part2/script.js
@@ -122,3 +122,11 @@ console.log(neighbours);
 1. Create an object called 'myCountry' for a country of your choice, containing
 properties 'country', 'capital', 'language', 'population' and
 'neighbours' (an array like we used in previous assignments)*/
+
+const myCountry = {
+    country: 'Finland',
+    capital: 'Helsinki',
+    language: 'finnish',
+    population: 6,
+    neighbours: ['Norway', 'Sweden', 'Russia']
+};
