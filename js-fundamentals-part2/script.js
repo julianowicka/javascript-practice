bfbfa1f1859fd271b3b8d066a34501abab52fd89
Julia Nowicka <julia.nowickaa@gmail.com>
2023-04-30 22:08:28 +0200

    Add task solution from "Dot vs. Bracket Notation" Lecture

--- js-fundamentals-part2/script.js
+++ js-fundamentals-part2/script.js
@@ -136,3 +136,14 @@ console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countrie
 and a capital called Helsinki.'
 2. Increase the country's population by two million using dot notation, and then
 decrease it by two million using brackets notation.*/
+
+console.log(
+    `${myCountry.country} has ${myCountry.population} million
+${myCountry.language}-speaking people,
+${myCountry.neighbours.length} neighbouring countries and
+a capital called ${myCountry.capital}.`
+);
+myCountry.population += 2;
+console.log(myCountry.population);
+myCountry['population'] -= 2;
+console.log(myCountry.population);
