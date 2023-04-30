64d7a8d1a246831b0d2485c664ae1b6457054f3c
Julia Nowicka <julia.nowickaa@gmail.com>
2023-04-30 22:07:54 +0200

    Add task description from "Dot vs. Bracket Notation" Lecture

--- js-fundamentals-part2/script.js
+++ js-fundamentals-part2/script.js
@@ -130,3 +130,9 @@ const myCountry = {
     population: 6,
     neighbours: ['Norway', 'Sweden', 'Russia']
 };
+/*LECTURE: Dot vs. Bracket Notation
+1. Using the object from the previous assignment, log a string like this to the
+console: 'Finland has 6 million finnish-speaking people, 3 neighbouring countries
+and a capital called Helsinki.'
+2. Increase the country's population by two million using dot notation, and then
+decrease it by two million using brackets notation.*/
