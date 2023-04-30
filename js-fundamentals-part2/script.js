ff434c8edeba9e173378f6a88d05821669b0901d
Julia Nowicka <julia.nowickaa@gmail.com>
2023-04-30 22:16:39 +0200

    Add task solution from "Object Methods" Lecture

--- js-fundamentals-part2/script.js
+++ js-fundamentals-part2/script.js
@@ -157,3 +157,28 @@ assignment, but this time using the 'this' keyword.
 method will set a new property on the object, called 'isIsland'.
 'isIsland' will be true if there are no neighbouring countries, and false if
 there are. Use the ternary operator to set the property.*/
+
+const myCountry = {
+    country: 'Finland',
+    capital: 'Helsinki',
+    language: 'finnish',
+    population: 6,
+    neighbours: ['Norway', 'Sweden', 'Russia'],
+    describe: function () {
+        console.log(
+            `${this.country} has ${this.population} million
+${this.language}-speaking people,
+${this.neighbours.length} neighbouring countries and a
+capital called ${this.capital}.`
+        );
+    },
+    checkIsland: function () {
+        this.isIsland = this.neighbours.length === 0 ? true :
+            false;
+// Even simpler version (see why this works...)
+// this.isIsland = !Boolean(this.neighbours.length);
+    }
+};
+myCountry2.describe();
+myCountry2.checkIsland();
+console.log(myCountry2);
