2cc83faf4a303f9c6ab10d1111e6f54870587bd1
Julia Nowicka <julia.nowickaa@gmail.com>
2023-05-01 21:27:54 +0200

    Add the task solution from 'Looping Arrays, Breaking and Continuing' Lecture

--- js-fundamentals-part2/script.js
+++ js-fundamentals-part2/script.js
@@ -198,3 +198,10 @@ function 'percentageOfWorld1' that you created earlier
 3. Confirm that 'percentages2' contains exactly the same values as the
 'percentages' array that we created manually in the previous assignment,
 and reflect on how much better this solution is*/
+const populations = [10, 1441, 332, 83];
+const percentages2 = [];
+for (let i = 0; i < populations.length; i++) {
+    const perc = percentageOfWorld1(populations[i]);
+    percentages2.push(perc);
+}
+console.log(percentages2);
